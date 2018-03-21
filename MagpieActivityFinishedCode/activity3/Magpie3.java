bfd80137734ba77c29a9cdaad9be1b30c61d8b84
DavidYunny <32076541+DavidYunny@users.noreply.github.com>
2018-03-21 11:34:09 -0700

    Update Magpie3.java

--- MagpieActivityFinishedCode/activity3/Magpie3.java
+++ MagpieActivityFinishedCode/activity3/Magpie3.java
@@ -30,23 +30,29 @@ public class Magpie3
 	public String getResponse(String statement)
 	{
 		String response = "";
-		if (statement.length() == 0)
-		{
-			response = "Say something, please.";
-		}
-		else if (findKeyword(statement, "no") >= 0)
-		{
+		String statementTrim = statement.trim();
+		
+		if (statement.indexOf("no") >= 0){
 			response = "Why so negative?";
-		}
-		else if (findKeyword(statement, "mother") >= 0
-				|| findKeyword(statement, "father") >= 0
-				|| findKeyword(statement, "sister") >= 0
-				|| findKeyword(statement, "brother") >= 0)
-		{
+		} else if (statement.indexOf("mother") >= 0 || statement.indexOf("father") >= 0 || statement.indexOf("sister") >= 0 || statement.indexOf("brother") >= 0){
 			response = "Tell me more about your family.";
-		}
-		else
-		{
+		} else if (statement.indexOf("Germany") >= 0 || statement.indexOf("Germany") >= 0){
+			response = "I used to live in Italy. We have pretty girls here";
+		} else if (statement.indexOf("dog") >= 0 || statement.indexOf("cat") >= 0 || statement.indexOf("pet") >= 0 || statement.indexOf("snake") >= 0){
+			response = "I also had some pets. But my Dog died 1 year ago.";
+		} else if (statement.indexOf("Germany") >= 0 || statement.indexOf("Germany") >= 0){
+			response = "I used to live in Italy. We have pretty girls here";
+		} else if (statement.indexOf("Mr") >= 0 || statement.indexOf("Mr.") >= 0){
+			response = "Oh Wait I also had him as my teacher!!";
+		} else if (statement.indexOf("Mrs") >= 0 || statement.indexOf("Mrs.") >= 0) {
+			response = "Wait!!!! I also had her as my teacher.";
+		} else if ((statement.indexOf("I") >= 0 || statement.indexOf("i") >= 0 )&& statement.indexOf("think") >= 0){
+			response = "Amen. LMAO";
+		} else if (!(statementTrim.length() >= 1)){
+			response = "You Serious Bro?";
+		} else if (statement.indexOf("food") >= 0){
+			response = "My favorite food is Korean BBQ!!!!!! LOVE THEM SO MUCH!!!!!!!!!!!!!!!!!!!!!<3<3";
+		} else {
 			response = getRandomResponse();
 		}
 		return response;
@@ -144,26 +150,31 @@ public class Magpie3
 	 */
 	private String getRandomResponse()
 	{
-		final int NUMBER_OF_RESPONSES = 4;
+		final int NUMBER_OF_RESPONSES = 9;
 		double r = Math.random();
-		int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
+		int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
 		String response = "";
-
-		if (whichResponse == 0)
-		{
+		
+		if (whichResponse == 0){
+			response = "WOWOWOWOWWOW!!!!";
+		} else if (whichResponse == 0){
 			response = "Interesting, tell me more.";
-		}
-		else if (whichResponse == 1)
-		{
+		} else if (whichResponse == 1){
 			response = "Hmmm.";
-		}
-		else if (whichResponse == 2)
-		{
+		} else if (whichResponse == 2){
 			response = "Do you really think so?";
-		}
-		else if (whichResponse == 3)
-		{
+		} else if (whichResponse == 3){
 			response = "You don't say.";
+		} else if (whichResponse == 4){
+			response = "You Smart Man";
+		} else if (whichResponse == 5){
+			response = "But Why....?";
+		} else if (whichResponse == 6){
+			response = "You don't say that to the Robots";
+		} else if (whichResponse == 7){
+			response = "Bra, Your Boned";
+		} else if (whichResponse == 8){
+			response = "LAMO you are a funny guy. :)))";
 		}
 
 		return response;
